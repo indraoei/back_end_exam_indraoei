ef6b7631d19c1bb0b04810948bd99880bf08eb82
indraoei <31429722+indraoei@users.noreply.github.com>
2017-10-12 12:14:14 +0700

    Initial commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\UnitRumah;
-use App\Customer;
 use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
